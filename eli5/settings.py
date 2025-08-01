92f786eebbd476a2da6c9f00c2271826ab9c1283
thakida-christy <alanjoseph1200@gmail.com>
2025-08-02 01:33:50 +0530

    Update settings.py

--- eli5/settings.py
+++ eli5/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*y+y-0$evq2&0%x)hc&(9=hb(+o*4deliyllh2zu+j$mo)^%(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
